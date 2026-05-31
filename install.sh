299cf4d6daa934ab895d4085da12162a4ff81948
clor-cli <clor-cli@users.noreply.github.com>
2026-05-31 21:21:08 +0000

    Pin install.sh to v1.0.0

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ set -o pipefail
 
 DEBUG="${DEBUG:-false}"
 SUPPORT_EMAIL="support@clor.com"
-DEFAULT_VERSION="v0.1.0"
+DEFAULT_VERSION="v1.0.0"
 VERSION="${CLOR_VERSION:-${DEFAULT_VERSION}}"
 INSTALL_DIR="${HOME}/.local/bin"
 EXE="${INSTALL_DIR}/clor"
