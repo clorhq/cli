c2e4c6f124566476b9a0b031d987f62d2a642f74
Clor <admin@clor.com>
2026-07-16 08:05:38 -0700

    bump version to 1.7.2

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ set -o pipefail
 
 DEBUG="${DEBUG:-false}"
 SUPPORT_EMAIL="support@clor.com"
-LATEST_VERSION="v1.7.1"
+LATEST_VERSION="v1.7.2"
 VERSION="${CLOR_VERSION:-${LATEST_VERSION}}"
 CLOR_INSTALL_FORCE="${CLOR_INSTALL_FORCE:-false}"
 if [[ "${CLOR_AUTOUPDATE:-}" == "" ]]; then
