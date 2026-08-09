bc138e82c2c7358335f29547996b70cfe233f620
clor-cli <clor-cli@users.noreply.github.com>
2026-08-09 15:02:37 -0700

    Pin install.sh to v1.9.32

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ set -o pipefail
 
 DEBUG="${DEBUG:-false}"
 SUPPORT_EMAIL="support@clor.com"
-LATEST_VERSION="v1.9.31"
+LATEST_VERSION="v1.9.32"
 VERSION="${CLOR_VERSION:-${LATEST_VERSION}}"
 CLOR_INSTALL_FORCE="${CLOR_INSTALL_FORCE:-false}"
 if [[ "${CLOR_AUTOUPDATE:-}" == "" ]]; then
